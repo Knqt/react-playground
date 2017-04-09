7f2b3de6a598ac2ccc57503d26ff3bc6c77a02ee
Knqt <knot.wittawat@gmail.com>
2017-04-09 09:54:57 +0700

    chapter 8 : bootstrap & JSS

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -13,10 +13,19 @@ import thunkMiddleware from 'redux-thunk'
 
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { APP_CONTAINER_SELECTOR, JSS_SSR_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
 import setUpSocket from './socket'
+
+import $ from 'jquery'
+import Tether from 'tether'
+
+// [right after all your imports]
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
@@ -49,6 +58,10 @@ if (module.hot) {
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+const jssServerSide = document.querySelector(JSS_SSR_SELECTOR)
+// flow-disable-next-line
+jssServerSide.parentNode.removeChild(jssServerSide)
 
 // [at the very end of the file]
 setUpSocket(store)
